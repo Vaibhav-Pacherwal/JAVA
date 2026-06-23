eb668d8a8610ae3696a42fd85f78338b2d52b097
Vaibhav-Pacherwal <vaibhavpacherwal57@gmail.com>
2026-06-24 03:14:19 +0530

    SM

--- spiralMatrix.java
+++ spiralMatrix.java
@@ -4,29 +4,21 @@ public class spiralMatrix {
         int i = 0, j = 0;
 
         while (i < rows && j < cols) {
-
-            // Top row
             for (int k = j; k < cols; k++) {
                 System.out.print(matrix[i][k] + " ");
             }
             i++;
-            if (i >= rows) break;
 
-            // Right column
             for (int l = i; l < rows; l++) {
                 System.out.print(matrix[l][cols - 1] + " ");
             }
             cols--;
-            if (j >= cols) break;
 
-            // Bottom row
             for (int m = cols - 1; m >= j; m--) {
                 System.out.print(matrix[rows - 1][m] + " ");
             }
             rows--;
-            if (i >= rows) break;
 
-            // Left column
             for (int n = rows - 1; n >= i; n--) {
                 System.out.print(matrix[n][j] + " ");
             }
